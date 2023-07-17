f181e8cb1ddd0f791f2e5ca58a993b04261c2c93
unknown <TshepoMagagulaTp11@gmail.com>
2023-07-17 12:48:06 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,11 @@
+function greet(name){
+return "Hello, " + name;
+}
+
+
+const msg = greet('Xola');
+const msg1 = greet('Nurha');
+
+console.log(msg);
+console.log(msg1);
+console.log(greet('Ntombi'));
